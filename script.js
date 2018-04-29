140f92d12dd56c818551b068aef40c8d3d87ebdd
Apeksha-Sharma <38656335+Apeksha-Sharma@users.noreply.github.com>
2018-04-29 16:00:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,77 @@
-(function(angular) {
-  'use strict';
-  angular.module('OMDbAPISearch', [])
-    .controller('searchMovies', ['$scope', '$http',
-      function($scope, $http) {
-        $scope.method = 'GET';
-        $scope.fetch = function() {
-          if ($scope.searchparam) {
-            $scope.url = 'https://www.omdbapi.com/?s=' + $scope.searchparam + '&type=movie&r=json';
-            $http({
-              method: $scope.method,
-              url: $scope.url
-            }).
-            then(function(response) {
-              if (response.data.Response) {
-                // success
-                $('.results').css('display', 'block');
-                $('.noResults').css('display', 'none');
-                var theSrchResults = response.data["Search"];
-                angular.forEach(theSrchResults, function(obj) {
-                  // loop through each movie, and pull the details using the IMDB ID
-                  $http({
-                    method: $scope.method,
-                    url: 'https://www.omdbapi.com/?i=' + obj.imdbID + '&plot=full&r=json&tomatoes=true'
-                  }).
-                  then(function(response) {
-                    // extend the details to the parent
-                    obj.details = response.data;
-                  });
-                });
-                $scope.movieResults = theSrchResults;
-              } else {
-                //error, movie not found
-                console.log("not found");
-                $('.results').css('display', 'none');
-                $('.noResults').css('display', 'block');
-                $('.noResults').html("<strong>No results found.</strong>");
-              }
-            }, function(response) {
-              console.log("failure");
-              $('.results').css('display', 'none');
-              $('.noResults').css('display', 'block');
-              $('.noResults').html("<strong>Network or data error, please try again.</strong>");
-            });
-          } else {
-            // no input value
-            $('.results').css('display', 'none');
-            $('.noResults').css('display', 'none');
-            $('#theSearch').fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-          }
-        };
-      }
-    ])
-    .directive('movieSrchResults', function() {
-      return {
-        templateUrl: 'movieResults.html'
-      };
+function show_movie_details() {
+  
+  // Grab our movie object
+  var movie = $(this);
+  
+  
+  // If the movie element has the "expanded" class, then we're already showing its data.
+  // So if it's been clicked, then the user is trying to hide the details.
+  // So hide those details!
+  if ( movie.hasClass("expanded") ) {
+    
+    movie
+      .removeClass("expanded")
+      .find(".movie-details-content").slideUp(200);
+    
+    
+  // Check if we've already fetched the data for this movie.
+  // If we have, we can simply reveal that data!
+  } else if ( movie.data("checked") ) {
+    
+    movie
+      .addClass("expanded")
+      .find(".movie-details-content").slideDown(200);
+    
+    
+  // If we're here, then we don't have data for this movie yet.
+  // So let's fetch it!
+  } else {
+
+    // Set our flag to show that we've fetched this movie's details (so we don't check for it twice).
+    // Also add the "expanded" class.
+    movie
+      .data("checked", "true")
+      .addClass("expanded");
+
+    // Append a new section to the .movie-details div, where we'll put the data that we fetch.
+    // Also add a "loading" spinner gif.
+    movie.find(".movie-details")
+      .append("<div class='movie-details-content'></div>")
+      .find(".movie-details-top").append("<img src='http://blimpage.com/pants/ajax-loader-222.gif' class='movie-details-loading'>");
+
+    // Grab the movie's IMDB ID, which is stored as a data- attribute.
+    var movie_imdb_id = movie.data("imdb-id"),
+
+    // Grab the div where we'll drop the details that we fetch.
+        movie_details = movie.find(".movie-details-content");
+        
+    // Make an AJAX call to the Open Movie Database, to grab this movie's details.
+    $.get("http://www.omdbapi.com/?i=" + movie_imdb_id)
+
+      // If the AJAX call was successful, let's process that data!
+      .done( function( movie_data ) {
+
+        // Remove the "loading" spinner.
+        movie.find(".movie-details-loading").remove();
+
+        // Drop some pieces of data onto the page, and slide the div open so we can see them.
+        movie_details.html(
+          "<h3>" + movie_data.Title + " (" + movie_data.Year + ")</h3>" +
+          "<p><strong>Genre: </strong> " + movie_data.Genre + "</p>" +
+          "<p><strong>Director: </strong> " + movie_data.Director + "</p>" +
+          "<p><strong>Starring: </strong> " + movie_data.Actors + "</p>" +
+          "<p><strong>Released: </strong> " + movie_data.Released + "</p>" +
+          "<p><strong>Plot: </strong> " + movie_data.Plot + "</p>"
+        ).slideDown(200);
+      })
+
+      // If the AJAX call was unsuccessful, display an error message.
+      .fail( function() {
+        movie_details.text("Sorry, something went wrong!")
     });
-})(window.angular);
+
+  }
+}
+
+// When a movie is clicked, run our function to get its details.
+$("#movies .movie").on("click.movie", show_movie_details);
